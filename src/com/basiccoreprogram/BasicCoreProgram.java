7515fdb44be2c971e457e267a84079d4548a5a83
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-02-28 23:34:31 +0530

    Added: Code for flipcoin and check percentage of head and tail

--- src/com/basiccoreprogram/BasicCoreProgram.java
+++ src/com/basiccoreprogram/BasicCoreProgram.java
@@ -1,7 +1,31 @@
 package com.basiccoreprogram;
 
+import java.util.Scanner;
+
 public class BasicCoreProgram {
     public static void main(String[] args) {
-        System.out.println("Welcome To Basics Core program");
+        // INITIALIZATION
+        int n, head_cnt = 0, tail_cnt = 0;
+        double heads, tails;
+
+        Scanner sc = new Scanner(System.in);
+
+        // INPUT DATA
+        System.out.print("enter number of times you want to flip the coin: ");
+        n = sc.nextInt();
+
+        // COMPUTATION
+        for (int i = 0; i < n; i++) {
+            double random = Math.random();
+            if (random < 0.5)
+                tail_cnt++;
+            else
+                head_cnt++;
+        }
+
+        heads = head_cnt / (double) n * 100;
+        tails = tail_cnt / (double) n * 100;
+        System.out.println("Percentage of heads: " + heads + "%");
+        System.out.println("Percentage of tails: " + tails + "%");
     }
 }

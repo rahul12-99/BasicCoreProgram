427714afe130a4ebdc937751a8dec9187ab6f028
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-02-28 23:59:37 +0530

    Added: Code for find the prime factor

--- src/com/basiccoreprogram/BasicCoreProgram.java
+++ src/com/basiccoreprogram/BasicCoreProgram.java
@@ -4,14 +4,19 @@ import java.util.Scanner;
 
 public class BasicCoreProgram {
     public static void main(String[] args) {
-        double nthHarmonic = 0;
-        System.out.println("Enter a number");
-        Scanner Sc = new Scanner(System.in);
-        int N = Sc.nextInt();
-        // Loop to apply formula
-        for (int i = 1; i <= N; i++){
-            nthHarmonic += (float)1/i;
+        int number;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        number = sc.nextInt();
+
+        for(int i = 2; i < number; i++) {
+            while(number%i == 0) {
+                System.out.println(i+" ");
+                number = number/i;
+            }
         }
-        System.out.println("Nth Harmonic Value is: " + nthHarmonic);
+        if(number > 2) {
+            System.out.println(number);
+        }
     }
 }

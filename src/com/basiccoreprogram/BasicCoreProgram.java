5c559720713946ba90222684f42177e0208c4027
rahul12-99/rahul12-99 <rahulkrrao162@gmail.com>
2023-02-28 19:03:54 +0530

    Added: Welcome message

---
+++ src/com/basiccoreprogram/BasicCoreProgram.java
@@ -0,0 +1,7 @@
+package com.basiccoreprogram;
+
+public class BasicCoreProgram {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Basics Core program");
+    }
+}
